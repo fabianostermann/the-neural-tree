f83af19fd1429cf243bdd7ed4dc9fe488c36dd8e
Fabian Ostermann <fabian.ostermann@tu-dortmund.de>
2026-08-09 23:39:46 +0200

    Implement NeuralTree manipulation functions and moved test code to Controller

--- modules/Controller.py
+++ modules/Controller.py
@@ -2,5 +2,19 @@ USES = [ "NeuralTree" ]
 REQUIRES = [ "Commands" ]
 PROVIDES = [ "NeuralTree" ]
 
+import random, time
+
 def _update(bb):
-    pass
+    print(bb.NeuralTree.TreeComponent.instances)
+    
+    random.choice([
+        bb.NeuralTree.mutate_neuron,
+        bb.NeuralTree.new_leaf,
+        bb.NeuralTree.thicken,
+        bb.NeuralTree.grow_branch,
+    ])()
+    print(bb.NeuralTree.TreeComponent.instances)
+    
+    bb.NeuralTree.tree.eval_subtree()
+    
+    time.sleep(.5)
--- representations/NeuralTree.py
+++ representations/NeuralTree.py
@@ -1,49 +1,59 @@
 import random
 
 def _validate():
-    n = Neuron(3)
-    print(n.forward([1,2,3]))
+    #n = Neuron(3)
+    #print(n.forward([1,2,3]))
     
     assert tree._children[0]._parent == tree
     
-    EXIT_SUCCESS
-    
 def thicken():
-    # TODO: Thicken root or branches
-    raise NotImplementedError("Not yet implemented")
+    # instanciate a new leaf at random position
+    branch = Leaf.instances[0]
+    while isinstance(branch, Leaf):
+        branch = random.choice(TreeComponent.instances)
+    branch.thicken()
     
 def mutate_neuron():
-    # TODO: choose random neuron and call mutate
-    raise NotImplementedError("Not yet implemented")
+    # choose random neuron and call mutate
+    random.choice(Neuron.instances).mutate()
     
 def new_leaf():
-    # TODO: instanciate a new leaf at random position
-    raise NotImplementedError("Not yet implemented")
+    # instanciate a new leaf at random position
+    branch = Leaf.instances[0]
+    while isinstance(branch, Leaf):
+        branch = random.choice(TreeComponent.instances)
+    branch.add_leaf()
     
-def grow():
-    # TODO: grow a new branch between a random leaf and its old parent
-    raise NotImplementedError("Not yet implemented")
-
+def grow_branch():
+    # grow a new branch between a random leaf and its old parent
+    leaf = tree
+    while not isinstance(leaf, Leaf):
+        leaf = random.choice(TreeComponent.instances)
+    leaf.grow_branch()
     
 class Neuron:
     instances = []
     
     def __init__(self, in_size: int, weight_range=[-0.5,0.5], bias_range=[-0.1,0.1]):
-        self.weights = [ random.uniform(*weight_range) ] * in_size
-        self.bias = random.uniform(*bias_range)
+        self._weights = [ random.uniform(*weight_range) ] * in_size
+        self._bias = random.uniform(*bias_range)
         self.activation = Neuron.relu
         Neuron.instances.append(self)
         
     def forward(self, x):
-        assert len(x) == len(self.weights), "input size does not match num of weights"
-        x = sum([ w_i*x_i for w_i, x_i in zip(self.weights, x)])
-        x = x + self.bias
+        assert len(x) == len(self._weights), "input size does not match num of weights"
+        x = sum([ w_i*x_i for w_i, x_i in zip(self._weights, x)])
+        x = x + self._bias
         self.activation(x)
         return x
         
     def relu(x):
         return max(0, x)
-    
+        
+    def mutate(self):
+        self._weights[random.randrange(len(self._weights))] += random.gauss(mu=0.0, sigma=1.0)
+        if random.random() < 0.2:
+            self._bias += random.gauss(mu=0.0, sigma=0.2)
         
 class TreeComponent:
     instances = []
@@ -52,19 +62,38 @@ class TreeComponent:
         self._neurons = [ Neuron(in_size) for o in range(out_size) ]
         self._parent = parent
         self._children = []
+        self._values = None
         TreeComponent.instances.append(self)
     
     def forward(self, x):
-        assert len(x) == self.get_in_size(), "input sizes do not match"
+        assert len(x) == self.get_in_size(), f"input sizes do not match: {self} {len(x)} {self.get_in_size()}"
         
         x = [ n.forward(x) for n in self._neurons ]
         return x
         
+    def eval_subtree(self):
+        x = self._parent.get_values()
+        self._values = self.forward(x)
+        for child in self._children:
+            child.eval_subtree()
+            
+    def get_values(self):
+        return self._values
+        
     def get_in_size(self):
-        return len(self._neurons[0].weights)
+        return len(self._neurons[0]._weights)
         
     def get_out_size(self):
         return len(self._neurons)
+        
+    def add_leaf(self):
+        self._children.append(Leaf(self.get_out_size(), parent=self))
+        
+    def thicken(self):
+        self._neurons.append(Neuron(self.get_in_size()))
+        for child in self._children:
+            for neuron in child._neurons:
+                neuron._weights.append(random.uniform(-0.1,0.1))
 
 class Root(TreeComponent):
     instance = None
@@ -73,19 +102,55 @@ class Root(TreeComponent):
         super().__init__(2,2)
         assert Root.instance is None, "Root exists already."
         
-        # make one initial leaf
-        self._children.append(Leaf(self.get_out_size(), parent=self))
+        # guarantee one initial leaf
+        self.add_leaf()
         
         Root.instance = self
         
+    def eval_subtree(self):
+        self._values = self.forward([1.0]*self.get_in_size())
+        for child in self._children:
+            child.eval_subtree()
+            
+    def thicken(self):
+        super().thicken()
+        for neuron in self._neurons:
+            neuron._weights.append(random.uniform(-0.1,0.1))
         
 class Leaf(TreeComponent):
     instances = []
+    DEFAULT_OUT_SIZE = 2
 
     def __init__(self, in_size: int, parent=None):
-        super().__init__(in_size, 2, parent=parent)
+        super().__init__(in_size, Leaf.DEFAULT_OUT_SIZE, parent=parent)
         assert parent
+        self._children = None
         Leaf.instances.append(self)
+        
+    def eval_subtree(self):
+        x = self._parent.get_values()
+        self._values = self.forward(x)
+        assert not self._children
+        
+    def add_leaf(self):
+        assert False, "Called add_leaf on a leaf instance."
+        
+    def thicken(self):
+        assert False, "Called thicken on a leaf instance"
+        
+    def grow_branch(self):
+        # create a brand new branch
+        new_branch = TreeComponent(self.get_in_size(), self.get_out_size(), parent=self._parent)
+        new_branch._children.append(self)
+        
+        # adjust neurons
+        new_branch._neurons = self._neurons
+        self._neurons = [ Neuron(new_branch.get_out_size()) for o in range(Leaf.DEFAULT_OUT_SIZE) ]
+        
+        # integrate new branch into tree
+        self._parent._children.append(new_branch)
+        self._parent._children.remove(self)
+        self._parent = new_branch
         
 
 tree = Root()
