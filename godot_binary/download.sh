7d9c811aeba062bb3b827519ad68fe15f59d0a2f
Fabian Ostermann <fabian.ostermann@tu-dortmund.de>
2026-08-15 22:06:14 +0200

    Moved binary info to run.sh

--- godot_binary/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Download binary here: https://downloads.godotengine.org/?version=4.7.1&flavor=stable&slug=linux.x86_64.zip&platform=linux.64"
--- run.sh
+++ run.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+GODOT_BINARY="godot_binary/Godot_v4.7.1-stable_linux.x86_64"
+
+if [ ! -f "$GODOT_BINARY" ];
+then
+  echo "Download godot binary first:"
+  echo "  -> https://downloads.godotengine.org/?version=4.7.1&flavor=stable&slug=linux.x86_64.zip&platform=linux.64"
+  echo "Then unzip and place godot binary at:" $GODOT_BINARY
+  exit
+fi
+
 if [ "$1" != "--dev" ];
 then
   cd godot_project
-  ../godot_binary/Godot_v4.7.1-stable_linux.x86_64 -f &
+  ../$GODOT_BINARY --resolution 1920x1080 &
   PID=$!
   cd ..
 
